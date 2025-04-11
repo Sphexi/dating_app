3363fce85f27f0478d5cdc95f70bb9a1922d8031
Sphexi <git@sphexi.com>
2025-04-11 09:24:40 -0700

    Update main.go

--- main.go
+++ main.go
@@ -25,6 +25,8 @@ import (
 func main() {
 	var err error
 	var db  *sql.DB
+	var portInt int
+	var psqlInfo string
 
 	// Get the db environment variables
 	dbHost := os.Getenv("DB_HOST")
@@ -32,12 +34,17 @@ func main() {
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("DB_NAME")
+	
+	portInt, err = strconv.Atoi(dbPort)
+	if err != nil {
+		log.Fatalf("Invalid DB_PORT: %s", dbPort)
+	}
 
 	psqlInfo = fmt.Sprintf("host=%s port=%d user=%s "+
     "password=%s dbname=%s sslmode=disable",
     dbHost, dbPort, dbUser, dbPassword, dbName)
 
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err = sql.Open("postgres", psqlInfo)
 	if err != nil {
 	  panic(err)
 	}
